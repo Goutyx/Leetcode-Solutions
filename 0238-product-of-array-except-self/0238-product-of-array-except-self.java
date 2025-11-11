bb601e7fd62c1451673b7162061f629efd5aa849
Goutam Pareek <148995885+Goutyx@users.noreply.github.com>
2025-11-12 01:22:56 +0530

    Time: 2 ms (88.8%), Space: 72.1 MB (6.08%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,29 +1,31 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] res = new int[n];
-        int totalProduct = 1;
-        int zeroCount = 0;
+        int n=nums.length;
+        int zero=0;
+        int count=1;
+        int arr[]= new int[n];
 
-        // Step 1: Calculate product and count zeros
-        for (int num : nums) {
-            if (num == 0) {
-                zeroCount++;
+        for(int i=0;i<n;i++){
+            if (nums[i]==0){
+                zero++;
                 continue;
             }
-            totalProduct *= num;
+            count=count*nums[i];
         }
 
-        // Step 2: Handle zero cases
-        for (int i = 0; i < n; i++) {
-            if (zeroCount > 1) res[i] = 0;
-            else if (zeroCount == 1) {
-                res[i] = (nums[i] == 0) ? totalProduct : 0;
-            } else {
-                res[i] = totalProduct / nums[i];
+        for(int i=0;i<n;i++){
+            if(zero>1){
+                arr[i]=0;
+            }else if(zero==1){
+                if(nums[i]==0){
+                    arr[i]=count;
+                }else{
+                    arr[i]=0;
+                }
+            }else{
+                arr[i]=count/nums[i];
             }
         }
-
-        return res;
+        return arr;
     }
 }
