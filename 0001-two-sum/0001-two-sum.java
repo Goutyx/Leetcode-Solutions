88eb8c3fd75d4e4b12754ff88511fe8f7b9e5f60
Goutam Pareek <148995885+Goutyx@users.noreply.github.com>
2025-11-17 16:19:38 +0530

    Time: 2 ms (99.16%), Space: 46.5 MB (12.93%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // OPTIMIZED CODE
         HashMap<Integer, Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
             int sum=target-nums[i];
@@ -14,17 +13,3 @@ class Solution {
         return new int[] {};
     }
 }
-//          BRUTE FORCE
-
-//          int[] arr=new int[2];
-//          for(int i=0;i<nums.length;i++){
-//              for(int j=i+1;j<nums.length;j++){
-//                  if(nums[i]+nums[j]==target){
-//                      arr[0]=i;
-//                      arr[1]=j;
-//                  }
-//              }
-//          }
-//     return arr;
-//     }
-// }
