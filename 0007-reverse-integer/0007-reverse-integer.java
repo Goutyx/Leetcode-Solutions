f0bcc21da2c6420d1a3ae07c4307b8f4a13b7267
Goutam Pareek <148995885+Goutyx@users.noreply.github.com>
2025-11-22 00:23:07 +0530

    Time: 1 ms (86.8%), Space: 42.6 MB (10.49%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,42 +1,19 @@
-// class Solution {
-//     public int reverse(int x) {
-//         int o=x;
-//         long n=0;
-//         if(x<0){
-//             x=-(x);
-//         }
-
-//         while(x!=0){
-//             int digit=x%10;
-//             n=n*10+digit;
-//             x/=10;
-//         }
-//         if(n>=(int)(Math.pow(2,31)-1)){
-//             return 0;
-//         }
-//         if(n<=(int)(Math.pow(-2,31))){
-//             return 0;
-//         }
-
-
-//         if(o>0){
-//             return (int)n;
-//         }else if(o<0){
-//             return -(int)(n);
-//         }else{
-//             return 0;
-//         }
-//     }
-// }
 class Solution {
     public int reverse(int x) {
-        long n = 0;
+        long n=0;
 
-        while (x != 0) {
-            n = n * 10 + x % 10;
-            x /= 10;
+        while(x!=0){
+            int digit=x%10;
+            n=n*10+digit;
+            x/=10;
         }
 
-        return (n < Integer.MIN_VALUE || n > Integer.MAX_VALUE) ? 0 : (int)n;
+        if(n>=(int)(Math.pow(2,31)-1)){
+            return 0;
+        }
+        if(n<=(int)(Math.pow(-2,31))){
+            return 0;
+        }
+        return (int)n;
     }
 }
