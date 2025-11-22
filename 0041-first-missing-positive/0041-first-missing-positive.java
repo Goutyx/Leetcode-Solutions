498d212c25d0d6719828d991ad1b070212882b87
Goutam Pareek <148995885+Goutyx@users.noreply.github.com>
2025-11-23 00:24:47 +0530

    Time: 20 ms (10.29%), Space: 74 MB (8.91%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -4,21 +4,13 @@ class Solution {
         int count = 1;
 
         for (int i = 0; i < nums.length; i++) {
-            // Skip negative numbers and zero
             if (nums[i] <= 0) continue;
-
-            // Skip duplicates
             if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            // If current number does not match expected count
             if (nums[i] != count) {
                 return count;
             }
-
-            count++; // Found the number we expect
+            count++; 
         }
-
-        // If everything matched, missing number is next
         return count;
     }
 }
