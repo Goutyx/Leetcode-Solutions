fb4977454a167923915df9ea0d0be4954fc558a1
Goutam Pareek <148995885+Goutyx@users.noreply.github.com>
2025-12-03 01:11:42 +0530

    Time: 27 ms (39%), Space: 47.5 MB (36.04%) - LeetHub

---
+++ 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minInsertions(String s) {
+        Stack <Character> stack = new Stack<>();
+        int add=0;
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch == '('){
+                stack.push(ch);
+            }else{
+                if((i+1)<s.length() && s.charAt(i+1)==')'){
+                    i++;
+                }else{
+                    add++;
+                }
+
+                if(stack.isEmpty()){
+                    add++;
+                }else{
+                    stack.pop();
+                }
+            }
+        }
+        add=add+2*stack.size();
+        return add;
+    }
+}
