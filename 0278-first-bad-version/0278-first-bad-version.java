f7b0d6875f838602d82e1202f0b9a208e79d287a
Goutam Pareek <148995885+Goutyx@users.noreply.github.com>
2026-01-29 12:46:11 +0530

    Time: 13 ms (99.62%), Space: 42.2 MB (26.57%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -3,12 +3,12 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int left=1;
-        int right=n;
-
+        int left = 1;
+        int right = n;
         while(left<right){
-            int mid=left+(right-left)/2;
-            if(isBadVersion(mid)){
+            int mid = left+(right-left)/2;
+            boolean found = isBadVersion(mid);
+            if(found==true){
                 right=mid;
             }else{
                 left=mid+1;
@@ -17,3 +17,41 @@ public class Solution extends VersionControl {
         return left;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // int left=1;
+        // int right=n;
+
+        // while(left<right){
+        //     int mid=left+(right-left)/2;
+        //     if(isBadVersion(mid)){
+        //         right=mid;
+        //     }else{
+        //         left=mid+1;
+        //     }
+        // }
+        // return left;
+//     }
+// }
