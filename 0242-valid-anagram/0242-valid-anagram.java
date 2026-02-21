acc62689a74dad9641e7861c3fd00770d552b858
Goutam Pareek <148995885+Goutyx@users.noreply.github.com>
2026-02-21 19:55:41 +0530

    Time: 4 ms (80.37%), Space: 46.9 MB (8.62%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,22 +3,24 @@ class Solution {
         char[] ss=s.toCharArray();
         char[] tt=t.toCharArray();
 
-        sorting(ss);
-        sorting(tt);
+        Arrays.sort(ss);
+        Arrays.sort(tt);
+        // sorting(ss);
+        // sorting(tt);
 
         return Arrays.equals(ss, tt);
     }
 
-    public void sorting(char[] arr){
-        int n = arr.length;
-        for(int i=1;i<n;i++){
-            char key = arr[i];
-            int j = i-1;
-            while(j>=0 && arr[j]>key){
-                arr[j+1]=arr[j];
-                j--;
-            }
-            arr[j+1] = key;
-        }
-    }
+    // public void sorting(char[] arr){
+    //     int n = arr.length;
+    //     for(int i=1;i<n;i++){
+    //         char key = arr[i];
+    //         int j = i-1;
+    //         while(j>=0 && arr[j]>key){
+    //             arr[j+1]=arr[j];
+    //             j--;
+    //         }
+    //         arr[j+1] = key;
+    //     }
+    // }
 }
