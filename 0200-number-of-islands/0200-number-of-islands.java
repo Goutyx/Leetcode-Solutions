e82be8bcf54a281c69371f3f89d0e85d224f77dc
Goutam Pareek <148995885+Goutyx@users.noreply.github.com>
2026-03-19 01:00:36 +0530

    Time: 3 ms (86.75%), Space: 52.4 MB (56.22%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -7,7 +7,7 @@ class Solution {
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
                 if(!visited[i][j] && grid[i][j]=='1'){
-                    bfs(i, j, grid, visited);
+                    dfs(i, j, grid, visited);
                     count++;
                 }
             }
@@ -15,33 +15,22 @@ class Solution {
         return count;
     }
 
-    public static void bfs(int i, int j, char[][] grid, boolean[][] visited){
-        int r = grid.length;
-        int c = grid[0].length;
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{i, j});
-        visited[i][j] = true;
-        while(!q.isEmpty()){
-            int[] temp = q.poll();
-            int first = temp[0];
-            int second = temp[1];
-
-            if(first-1>=0 && !visited[first-1][second] && grid[first-1][second]=='1'){
-                visited[first-1][second]=true;
-                q.add(new int[]{first-1,second});
-            }
-            if(first+1<r && !visited[first+1][second] && grid[first+1][second]=='1'){
-                visited[first+1][second]=true;
-                q.add(new int[]{first+1,second});
-            }
-            if(second-1>=0 && !visited[first][second-1] && grid[first][second-1]=='1'){
-                visited[first][second-1]=true;
-                q.add(new int[]{first,second-1});
-            }
-            if(second+1<c && !visited[first][second+1] && grid[first][second+1]=='1'){
-                visited[first][second+1]=true;
-                q.add(new int[]{first,second+1});
-            }
+    public static void dfs(int r, int c, char[][] grid, boolean[][] visited){
+        int rows = grid.length;
+        int cols = grid[0].length;
+        visited[r][c]=true;
+        
+        if(r-1>=0 && !visited[r-1][c] && grid[r-1][c]=='1'){
+            dfs(r-1,c,grid,visited);
+        }
+        if(c-1>=0 && !visited[r][c-1] && grid[r][c-1]=='1'){
+            dfs(r,c-1,grid,visited);
+        }
+        if(r+1<rows && !visited[r+1][c] && grid[r+1][c]=='1'){
+            dfs(r+1,c,grid,visited);
+        }
+        if(c+1<cols && !visited[r][c+1] && grid[r][c+1]=='1'){
+            dfs(r,c+1,grid,visited);
         }
     }
 }
